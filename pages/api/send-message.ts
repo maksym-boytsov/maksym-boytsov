c37cf2c7e4145c0acc20325420414d0ca2c7475e
Max ⚡️ <maksym.boytsov@gmail.com>
2022-11-24 00:32:05 +0100

    fix: message in one log

--- pages/api/send-message.ts
+++ pages/api/send-message.ts
@@ -3,8 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, message, name } = req.body;
 
-  console.info(`New message from ${name} <${email}>`);
-  console.info(message);
+  console.info(`New message from ${name} <${email}>: ${message}`);
 
   res.status(200).json({ message: "Success" });
 };
