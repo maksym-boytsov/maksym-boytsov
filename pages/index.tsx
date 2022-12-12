9a00ffa86f08344a2194266cf9740952ab7a75ae
Max ⚡️ <maksym.boytsov@gmail.com>
2022-12-12 13:18:07 +0100

    fix: disabled state for button

--- pages/index.tsx
+++ pages/index.tsx
@@ -393,6 +393,7 @@ export default function Home() {
             )}
           </div>
           <button
+            disabled={contactForm.formState.isSubmitting}
             type="submit"
             className="place-self-end inline-flex items-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
           >
