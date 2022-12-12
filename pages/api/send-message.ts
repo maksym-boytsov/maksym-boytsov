2421302b4a295ca976b8712467037512db72facb
Max ⚡️ <maksym.boytsov@gmail.com>
2022-12-12 13:16:46 +0100

    fix: log to see the email

--- pages/api/send-message.ts
+++ pages/api/send-message.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { email, message, name } = req.body;
 
-  console.info(`New message from ${name} <${email}>`);
+  console.info(`New message from ${name} | ${email}`);
   console.info(message);
 
   res.status(200).json({ message: "Success" });
