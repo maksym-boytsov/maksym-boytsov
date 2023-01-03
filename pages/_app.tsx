d9bd5520c85558d705c75a6a91078a2f265ed638
Max ⚡️ <maksym.boytsov@gmail.com>
2023-01-03 17:17:33 +0100

    feat/vercel-analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
