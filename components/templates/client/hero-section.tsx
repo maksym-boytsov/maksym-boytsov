67a348b473c3f206a1f6a70c371c283a9afdb063
Max ⚡️ <maksym.boytsov@gmail.com>
2023-04-08 17:04:50 +0200

    feat: hero section
    Signed-off-by: Max ⚡️ <maksym.boytsov@gmail.com>

--- components/templates/client/hero-section.tsx
+++ components/templates/client/hero-section.tsx
@@ -1,66 +1,65 @@
 "use client";
-import {
-  ArrowDownIcon,
-  ChatBubbleBottomCenterTextIcon,
-  PaintBrushIcon,
-} from "@heroicons/react/24/solid";
+import { ChatBubbleBottomCenterTextIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import React, { useEffect } from "react";
-import shadowSrc from "public/shadow.jpg";
-import { hotjar } from "react-hotjar";
+import fullBodySrc from "public/full-body.jpg";
+// import { hotjar } from "react-hotjar";
 
 import { Button } from "../../shared";
 
 const HeroSection = () => {
-  useEffect(() => {
-    hotjar.initialize(3442508, 6);
-  }, []);
+  // useEffect(() => {
+  //   hotjar.initialize(3442508, 6);
+  // }, []);
 
   return (
     <section
       id="hero"
       className="relative bg-zinc-900 h-screen grid md:grid-cols-2 grid-flow-col border-b border-zinc-700"
     >
+      <div className="bg-[url('/full-body.jpg')] bg-center bg-cover md:bg-none">
+        <div className="md:backdrop-filter-none backdrop-blur-sm backdrop-brightness-50 h-screen grid gap-2 px-4 text-center justify-center items-center justify-items-center content-center">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center">
+            {keywords.map((keyword, index) => {
+              return (
+                <p
+                  key={keyword.name}
+                  style={{ animationDelay: `${index}s` }}
+                  className={`mt-1 animate-[gradient-fade_4s_ease-out_infinite] bg-[length:600%] bg-bottom bg-gradient-to-r ${keyword.fromColor} ${keyword.viaColor} to-stone-500 text-transparent bg-clip-text font-bold tracking-tight text-5xl lg:text-6xl`}
+                >
+                  {keyword.name}
+                </p>
+              );
+            })}
+          </div>
+
+          <p className="mx-auto my-3 max-w-xl text-lg text-zinc-200 md:text-zinc-300">
+            Hey, I'm <span className="font-bold">Maksym</span>, a software
+            engineer and entrepreneur. I'm passionate about engineering products
+            that boost productivity and help people achieve their goals.
+          </p>
+
+          {/* @ts-ignore */}
+          <Button as="a" href="/#ai" type="button">
+            <ChatBubbleBottomCenterTextIcon
+              className="-ml-1 mr-2 h-5 w-5"
+              aria-hidden="true"
+            />
+            Know Me Better
+          </Button>
+        </div>
+      </div>
+
       <div className="hidden md:block relative">
         <Image
-          alt="Man walking towards his shadow."
-          src={shadowSrc}
+          alt="Maksym Boytsov pointing to mountains."
+          src={fullBodySrc}
           placeholder="blur"
           quality={75}
           className="w-full h-full absolute bottom-0 right-0 left-0 top-0 object-cover"
           fill
           priority
         />
-      </div>
-      <div className="text-center h-screen grid gap-2 justify-center items-center justify-items-center content-center px-4">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center">
-          {keywords.map((keyword, index) => {
-            return (
-              <p
-                key={keyword.name}
-                style={{ animationDelay: `${index}s` }}
-                className={`mt-1 animate-[gradient-fade_4s_ease-out_infinite] bg-[length:600%] bg-bottom bg-gradient-to-r ${keyword.fromColor} ${keyword.viaColor} to-stone-500 text-transparent bg-clip-text font-bold tracking-tight text-5xl lg:text-6xl`}
-              >
-                {keyword.name}
-              </p>
-            );
-          })}
-        </div>
-
-        <p className="mx-auto my-3 max-w-xl text-lg text-gray-400">
-          Hey, I'm <span className="font-bold">Maksym</span>, a software
-          engineer and entrepreneur. I'm passionate about engineering products
-          that boost productivity and help people achieve their goals.
-        </p>
-
-        {/* @ts-ignore */}
-        <Button as="a" href="/#ai" type="button">
-          <ChatBubbleBottomCenterTextIcon
-            className="-ml-1 mr-2 h-5 w-5"
-            aria-hidden="true"
-          />
-          Know Me Better
-        </Button>
       </div>
     </section>
   );
